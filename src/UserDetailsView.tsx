cdd19de5a9b007030f1b06054850513c5822ea8d
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-12-12 19:42:38 +0100

    Clean the code before deploy

--- src/UserDetailsView.tsx
+++ src/UserDetailsView.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 interface GetUserResponse {
   email: string;
 }
+
 interface GetBankAccountsResponse {
   accounts: Array<{
     // Typ number uzyty dla uproszczenia zadania
@@ -16,33 +18,27 @@ export const UserDetailsView = (props: UserDetailsViewProps) => {
   // Implementation...
   const [user, setUser] = useState<GetUserResponse | null>(null)
   const [account, setAccount] = useState<GetBankAccountsResponse | null>(null)
-  console.log(user)
   useEffect(() => {
     const fetchUser = async () => {
-      const userObj =  {email: 'julia@example.com'}
-      await setTimeout(() => {
-          setUser(userObj)
-        }, 3000);
+      //const userObj = await axios'/api/user'
+      const userObj = await axios('https://mocki.io/v1/d053a495-3838-41d3-a5ba-cca3704f1fd1');
+      setUser(userObj.data)
     };
     const fetchAccounts = async () => {
-        const accountsObj =  {accounts: [{balance: 12345}, {balance: 54321}, {balance: 13153}, {balance: 12435}]}
-        await setTimeout(() => {
-            setAccount(accountsObj)
-          }, 3000);
-      };
+      //const accountObj = await axios'/api/bank-accounts/'
+      const accountsObj = await axios('https://mocki.io/v1/2274d0df-5897-471e-b16d-f79ac215abb0');
+      setAccount(accountsObj.data)
+    };
     fetchUser();
     fetchAccounts();
   }, []);
-
   return (
     user === null || account === null ?
-    <h1>Loading</h1>
+    <h1>Loading...</h1>
     :
     <div>
-        <h1>User is{user?.email}</h1>
-        {account.accounts.map((item) => (
-            <p>{item.balance}</p>
-        ))}
+        <h1>User: {user?.email}</h1>
+        <p>Bank account: {account.accounts[0].balance}</p>
     </div>
   )
 };
